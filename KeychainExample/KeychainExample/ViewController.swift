5472a21e0f2ec7e758c0ea0e2f1cd4d3a291d303
Senthil kumar K <nightrider.cbe@gmail.com>
2020-05-07 12:06:46 +0530

    Commit

---
+++ KeychainExample/KeychainExample/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  KeychainExample
+//
+//  Created by SENTHILKUMAR on 07/05/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var InputField: UITextField!
+    
+    @IBOutlet var DisplayLbl: UILabel!
+    
+    //MARK: Save
+    @IBAction func Save_TouchUpInside(_ sender: Any) {
+        
+        
+    }
+    
+    //MARK: Update
+    @IBAction func Update_TouchUpInside(_ sender: Any) {
+        
+        
+    }
+    
+    //MARK: Delete
+    @IBAction func Delete_TouchUpInside(_ sender: Any) {
+    
+    
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
